a8a9d5302aaeb5ea9cbe54cd9e6be5bc538e2059
Giridhar-Akula <giridharyadav20000104@gmail.com>
2023-02-21 15:21:31 +0530

    first commit

---
+++ forms/application.js
@@ -0,0 +1,10 @@
+const form = document.getElementById('application-form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  
+  // Code to handle form submission goes here
+  
+  
+  alert('Job Applied successfully!');
+});
